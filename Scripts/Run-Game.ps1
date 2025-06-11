f70add708d6a6ba8d3564761bc8b2eccbc595640
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2025-06-12 00:01:09 +0200

    Automation (#5)
    * Add automation
    
    * Update templates
    
    * Grote changes
    
    * Update build script so it fetches before build

--- Scripts/Run-Game.ps1
+++ Scripts/Run-Game.ps1
@@ -1,16 +1,26 @@
 param ($githubToken)
 $solutionPath = Resolve-Path "$PSScriptRoot/../Source/NasdaqTraderSystem.sln";
 
+$startPath = Resolve-Path "$PSScriptRoot";
+Set-Location -Path "$PSScriptRoot/../"
+Write-Host "fetch and pull latest changes from the repository"
+git fetch
+git pull
+Set-Location -Path $startPath
+
+Write-Host "Restore and build the solution"
 #restore and build the solution
 dotnet restore $solutionPath
 dotnet build $solutionPath
 
+Write-Host "Restore and build all bots"
 #restore and build all bots
 Get-ChildItem -Path "$PSScriptRoot/../Bots" -Recurse -Filter *.csproj | ForEach-Object {
     dotnet restore $_.FullName
     dotnet build $_.FullName
 }
 
+Write-Host "Run the game"
 $exePath = Resolve-Path "$PSScriptRoot/../Build/NasdaqTrader.CLI.exe";
 
 $dataFolderPath = Resolve-Path "$PSScriptRoot/../Data";
@@ -18,6 +28,7 @@ Start-Process -FilePath $exePath -ArgumentList "-t 10000 -d $dataFolderPath -n 1
 
 Set-Location -Path "$PSScriptRoot/../Build/results"
 
+Write-Host "Push results to the repository"
 git add .
 git commit -m "Automated commit of build results"
 git push https://$githubToken@github.com/CSHDJO/NasdaqResults.git HEAD:main
