b0eb381314e1a696247db26f32e3d38e2b837112
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2025-06-12 11:17:43 +0200

    Update (#7)
    Co-authored-by: Hendrik de Jonge <h.de.jonge@chipsoft.com>

---
+++ Bots/HendrikBot/DeJongeTrader.cs
@@ -0,0 +1,29 @@
+﻿using NasdaqTrader.Bot.Core;
+
+namespace HendrikBot;
+
+public class DeJongeTrader : ITraderBot
+{
+    public string CompanyName => "de Jonge Investments";
+
+    public async Task DoTurn(ITraderSystemContext systemContext)
+    {
+        var listings = systemContext.GetListings();
+
+        foreach (var holding in systemContext.GetHoldings(this))
+        {
+            systemContext.SellStock(this, holding.Listing, holding.Amount);
+        }
+        
+        var tradeListings = listings
+            .OrderBy(c =>
+                c.PricePoints.FirstOrDefault(p => p.Date == systemContext.CurrentDate).Price -
+                c.PricePoints.FirstOrDefault(p => p.Date == systemContext.CurrentDate.AddDays(1)).Price).Take(2);
+
+        foreach (var listing in tradeListings)
+        {
+            systemContext.BuyStock(this, listing, 5);
+        }
+
+    }
+}
