dde8106c36922766a984af47e05bad5887dd6950
CS-Aleix <134361921+CS-Aleix@users.noreply.github.com>
2025-06-20 11:16:05 +0200

    First Fork PR (#14)
    * Fork basics
    
    * First fork
    
    Test
    
    * update basic bot
    
    * No gitignore
    
    Whoops
    
    ---------
    
    Co-authored-by: Lance-88 <129681882+Lance-88@users.noreply.github.com>

---
+++ Bots/AleixBot/AleixBot.cs
@@ -0,0 +1,18 @@
+﻿using NasdaqTrader.Bot.Core;
+
+namespace AleixBot;
+
+public class AleixBot : ITraderBot
+{
+    public string CompanyName => "Beginner Investments";
+
+    public async Task DoTurn(ITraderSystemContext systemContext)
+    {
+        var listings = systemContext.GetListings();
+        var cash = systemContext.GetCurrentCash(this);
+        var currentDate = systemContext.CurrentDate;
+        var tradesLeft = systemContext.GetTradesLeftForToday(this);
+
+        systemContext.BuyStock(this, listings[0], 1);
+    }
+}
