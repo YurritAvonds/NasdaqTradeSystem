868a9e7795f9e12aac74da888361fb9d458b374d
YurritAvonds <yurrit.avonds@gmail.com>
2025-07-25 20:53:44 +0200

    Separated Logger

---
+++ Bots/YurritBot/Logger.cs
@@ -0,0 +1,10 @@
+﻿namespace YurritBot;
+
+public class Logger
+{
+    public void LogToFile(string text)
+    {
+        var logFilePath = Path.Combine(AppContext.BaseDirectory, "yurritbot_errors.log");
+        File.AppendAllText(logFilePath, $"{text}{Environment.NewLine}");
+    }
+}
--- Bots/YurritBot/YurritTrader.cs
+++ Bots/YurritBot/YurritTrader.cs
@@ -4,27 +4,29 @@ namespace YurritBot;
 
 public class YurritBot : ITraderBot
 {
-    
     private const int cashLowerLimit = 10;
 
     public string CompanyName => "Stock Out Like a Sore Thumb";
 
     public async Task DoTurn(ITraderSystemContext systemContext)
     {
-        LogToFile($"{systemContext.CurrentDate}");
+        var logger = new Logger();
+        logger.LogToFile($"{systemContext.CurrentDate}");
 
         int indexToday = DetermineTodayIndex(systemContext);
         int indexYesterday = indexToday - 1;
         int indexTomorrow = indexToday + 1;
 
-        ExecuteSellStrategy(systemContext, indexToday, indexTomorrow);
-        ExecuteBuyStrategy(systemContext, indexToday, indexTomorrow);
+        ExecuteSellStrategy(systemContext, indexToday, indexTomorrow, logger);
+        ExecuteBuyStrategy(systemContext, indexToday, indexTomorrow, logger);
     }
 
-    private void ExecuteSellStrategy(ITraderSystemContext systemContext, int indexToday, int indexTomorrow)
+    private void ExecuteSellStrategy(ITraderSystemContext systemContext, int indexToday, int indexTomorrow, Logger logger)
     {
+        // TODO some are sold with losses because original price is not taken into account
         var sellableHoldings = systemContext.GetHoldings(this).Where(holding
-            => (holding.Listing.PricePoints[indexTomorrow].Price - holding.Listing.PricePoints[indexToday].Price) < 0);
+            => holding.Amount > 0
+                && ((holding.Listing.PricePoints[indexTomorrow].Price - holding.Listing.PricePoints[indexToday].Price) < 0));
 
         foreach (var holding in sellableHoldings)
         {
@@ -32,12 +34,12 @@ public class YurritBot : ITraderBot
 
             if (!success)
             {
-                LogToFile($"Failed SELL - Current {holding.Listing.Name} | Price {holding.Listing.PricePoints[indexToday].Price} | Amount {holding.Amount}");
+                logger.LogToFile($"Failed SELL - Current {holding.Listing.Name} | Price {holding.Listing.PricePoints[indexToday].Price} | Amount {holding.Amount}");
             }
         }
     }
 
-    private void ExecuteBuyStrategy(ITraderSystemContext systemContext, int indexToday, int indexTomorrow)
+    private void ExecuteBuyStrategy(ITraderSystemContext systemContext, int indexToday, int indexTomorrow, Logger logger)
     {
         var listingsByExpectedIncrease = systemContext.GetListings().OrderBy(listing
             => (listing.PricePoints[indexTomorrow].Price - listing.PricePoints[indexToday].Price) / listing.PricePoints[indexToday].Price);
@@ -64,15 +66,9 @@ public class YurritBot : ITraderBot
             var success = systemContext.BuyStock(this, listing, maxBuyAmount);
             if (!success)
             {
-                LogToFile($"Failed BUY - Current {currentCash} | Price {pricePointToday} | Amount {maxBuyAmount}");
+                logger.LogToFile($"Failed BUY - Current {currentCash} | Price {pricePointToday} | Amount {maxBuyAmount}");
             }
         }
-    }
-
-    private static void LogToFile(string text)
-    {
-        var logFilePath = Path.Combine(AppContext.BaseDirectory, "yurritbot_errors.log");
-        File.AppendAllText(logFilePath, $"{text}{Environment.NewLine}");
     }
 
     private static int DetermineTodayIndex(ITraderSystemContext systemContext)
@@ -80,6 +76,4 @@ public class YurritBot : ITraderBot
         return (int)(systemContext.CurrentDate.ToDateTime(TimeOnly.MinValue)
             - systemContext.StartDate.ToDateTime(TimeOnly.MinValue)).TotalDays;
     }
-
-    
 }
