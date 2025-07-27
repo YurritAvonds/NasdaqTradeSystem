b2da2b4ce4fe69d604973913814e08e8693296b0
YurritAvonds <yurrit.avonds@gmail.com>
2025-07-27 19:10:57 +0200

    DateCalculator

---
+++ Bots/YurritBot/DateCalculator.cs
@@ -0,0 +1,37 @@
+﻿using NasdaqTrader.Bot.Core;
+
+namespace YurritBot
+{
+    public class DateCalculator
+    {
+        public int CalculateBusinessDaysBetween(DateOnly startDate, DateOnly endDate)
+        {
+            DateTime startDateTime = startDate.ToDateTime(TimeOnly.MinValue);
+            DateTime endDateTime = endDate.ToDateTime(TimeOnly.MinValue);
+
+            int totalDays = (endDateTime - startDateTime).Days;
+            if (totalDays < 0)
+            {
+                return 0;
+            }
+
+            int businessDays = 0;
+            for (int i = 0; i < totalDays; i++)
+            {
+                DateTime currentDate = startDateTime.AddDays(i);
+                if (currentDate.DayOfWeek != DayOfWeek.Saturday &&
+                    currentDate.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    businessDays++;
+                }
+            }
+
+            return businessDays;
+        }
+
+        public int DetermineDateIndex(DateOnly date, IEnumerable<IPricePoint> pricePoints)
+            => pricePoints.Select((pricePoint, index) => (pricePoint, index))
+                .First(pointIndexPair => pointIndexPair.pricePoint.Date.Equals(date))
+                .index;
+    }
+}
---
+++ Bots/YurritBotTests/DateCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YurritBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace YurritBot.Tests
+{
+    [TestFixture]
+    public class DateCalculatorTests
+    {
+        [TestCase("01-01-2025", "01-01-2025", 0)]
+        [TestCase("01-01-2025", "01-03-2025", 2)]
+        [TestCase("01-01-2025", "01-08-2025", 5)]
+        [TestCase("01-01-2025", "01-13-2025", 8)]
+        public void CalculateBusinessDaysBetweenTest(DateOnly startDate, DateOnly endDate, int expectedIndex)
+        {
+            // Arrange
+            var dateCalculator = new DateCalculator();
+
+            // Act
+            var index = dateCalculator.CalculateBusinessDaysBetween(startDate, endDate);
+
+            // Assert
+            index.Should().Be(expectedIndex);
+        }
+    }
+}
